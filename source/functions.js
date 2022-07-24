a661cb981525f882dc96a2e3a26c9864b48fec1e
LINXF520 <50787822+LINXF520@users.noreply.github.com>
2022-07-25 00:08:27 +0800

    Add files via upload

--- source/functions.js
+++ source/functions.js
@@ -34,35 +34,23 @@ $(window).resize(function() {
 })(jQuery);
 
 function timeElapse(date){
-	// var current = Date();
-	// // var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	// var seconds = (Date.parse(date) - Date.parse(current)) / 1000;
-	// var days = Math.floor(seconds / (3600 * 24));
-	// seconds = seconds % (3600 * 24);
-	// var hours = Math.floor(seconds / 3600);
-	// if (hours < 10) {
-	// 	hours = "0" + hours;
-	// }
-	// seconds = seconds % 3600;
-	// var minutes = Math.floor(seconds / 60);
-	// if (minutes < 10) {
-	// 	minutes = "0" + minutes;
-	// }
-	// seconds = seconds % 60;
-	// if (seconds < 10) {
-	// 	seconds = "0" + seconds;
-	// }
-
-	var endtime = new Date("2020/7/25,9:00:00"); //结束时间
-	var nowtime = new Date(); //当前时间
-	var lefttime = parseInt((endtime.getTime() - nowtime.getTime()) / 1000);
-	days = parseInt(lefttime / 3600 / 24);
-	hours = parseInt((lefttime / 3600) % 24);
-	minutes = parseInt((lefttime / 60) % 60);
-	seconds = parseInt(lefttime % 60)
-
-
-
+	var current = Date();
+	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var days = Math.floor(seconds / (3600 * 24));
+	seconds = seconds % (3600 * 24);
+	var hours = Math.floor(seconds / 3600);
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	seconds = seconds % 3600;
+	var minutes = Math.floor(seconds / 60);
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	seconds = seconds % 60;
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
 	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
 	$("#clock").html(result);
 }
